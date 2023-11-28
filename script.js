13f01108e75476c65d91f19e805d8c6d13814a8c
Satyam <satyamworks20@gmail.com>
2023-11-28 13:51:48 +0530

    cart functionality added

--- script.js
+++ script.js
@@ -1,58 +1,77 @@
 let cart =document.getElementById('cart-item-no');
 let cartbutton = document.getElementsByClassName('add-to-cart')
+let cartDiv = document.getElementById('cartDiv')
 
+//making an arrry named items and inside that we put objects with full details
     let items=[
-        { Itemname: "This was our pact",quantity: 0,dollars: 7,cents: 49 ,src:"./assets/book1.png"},
-        { Itemname: "The famous five",quantity: 0,dollars: 4,cents: 59 ,src:"./assets/book2.png" },
-        { Itemname: "Matilda",quantity: 0,dollars: 4,cents: 59 ,src:"./assets/book3.png"},
-        { Itemname: "Harry Potter",quantity: 0,dollars: 6,cents: 80 ,src:"./assets/book4.png"},
-        { Itemname: "For Young Readers",quantity: 0,dollars: 7,cents: 29 ,src:"./assets/book5.png"},
-        { Itemname: "Wimpy kid - DIY",quantity: 0,dollars: 4,cents: 99 ,src:"./assets/book6.png"},
-        { Itemname: "DART Board",quantity: 0,dollars: 17,cents: 49 ,src:"./assets/dart-board.png"},
-        { Itemname: "Connect Four",quantity: 0,dollars: 19,cents: 99,src:"./assets/connect-four.png" },
-        { Itemname: "Jenga",quantity: 0,dollars: 20,cents: 99,src:"./assets/jenga.png"  },
-        { Itemname: "Monopoly",quantity: 0,dollars: 19,cents: 49 ,src:"./assets/monopoly.png" },
-        { Itemname: "Bookmarks",quantity: 0,dollars: 12,cents: 49 ,src:"./assets/bookmarks.png" },
-        { Itemname: "Birthday Card",quantity: 0,dollars: 12,cents: 49,src:"./assets/birthday-card.png"  },
-        { Itemname: "Stuffed toys",quantity: 0,dollars: 15,cents: 99 ,src:"./assets/stuffed-toy.png" },
-        { Itemname: "Dream catcher drawing",quantity: 0,dollars: 18,cents: 49,src:"./assets/dream-catcher.png"  },
+        { Itemname: "This was our pact",quantity: 0,price:7.48},
+        { Itemname: "The famous five",quantity: 0,price:4.59 },
+        { Itemname: "Matilda",quantity: 0,price:4.59 },
+        { Itemname: "Harry Potter",quantity: 0,price:6.80},
+        { Itemname: "For Young Readers",quantity: 0,price:7.29},
+        { Itemname: "Wimpy kid - DIY",quantity: 0,price:4.99},
+        { Itemname: "DART Board",quantity: 0,price:17.49},
+        { Itemname: "Connect Four",quantity: 0,price:17.99},
+        { Itemname: "Jenga",quantity: 0,price:20.99},
+        { Itemname: "Monopoly",quantity: 0,price:19.49},
+        { Itemname: "Bookmarks",quantity: 0,price:6.49},
+        { Itemname: "Birthday Card",quantity: 0,price:8.49},
+        { Itemname: "Stuffed toys",quantity: 0,price:3.99},
+        { Itemname: "Dream catcher drawing",quantity: 0,price:14.49},
     ];
-//main addition happen is this , it wil add all quantity  here no of //
-// function cartupdate(){ 
-//     let addquantity = 0;
-//     for(let i=0;i<items.length;i++){
-//         addquantity+= items[i].quantity      
-//     }
-//     cart.innerText=addquantity;
-    
 
-//     for(let a=0;a<cartbutton.length;a++){
-//         cartbutton[a].onclick=(e)=>{
-//             items[a].quantity++;
-//             cartupdate()
-//         }
-//     }
-
+let user = []
 let count = 0;
 cart.innerHTML = "0";
 for (let i = 0; i < cartbutton.length; i++){
+    //adding eventlistener for click to increase qunatity and count in cart option
 cartbutton[i].addEventListener("click", () => {
     count++;
     cart.innerHTML = count;    
-
     items[i].quantity++;
 })
 
 }
 
-    //adding quantity for each for them , how many times we click cart//
+
+let cartArray =[]
+let total =0
+let str =""
+
+//using onlick to show details of cart in console
+
+cartDiv.onclick=()=>{
+    str = ""
+    // console.log('hello')
+    //pushing selected item in cart array
+    items.forEach((item,index)=>{
+    if(item.quantity>0){
+        cartArray.push(item)
+    }
+})
+ //feching items from cart array for displaying it 
+    cartArray.forEach((item)=>{
+        str += `Item name: ${item.Itemname} - Quantity: ${item.quantity} `
+        total += item.price * item.quantity
+    })
+
+//displaying those things in console
+
+    console.log(str)
+    console.log(`Total Price: ${total}`)
+    console.log(`Quantity: ${count}`)
+
+//clearing alll detatils so that when we reclick on cart button then it should now add up again and again 
+
+    str=""
+    total=0
+    cartArray=[]
+}
 
 
 
 
 
-
-console.log("hello");
 
 
 
